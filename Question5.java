0c788082096249c153987320d622e2455f3d1f8d
PrimaryHeap <61671781+PrimaryHeap@users.noreply.github.com>
2020-04-24 12:14:57 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,12 +1,12 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class Question5
-{
-  public static void main(String[] args)
-  {
+public class Question5 {
+
+  public static void main(String[] args) {
     /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
+     * Prompt the user for number. This input indicates the number of integers the user will be entering next.
+     * Print out the mode (highest occurrence) from the set of integers.
      *    e.g.
      *     > 5
      *     > 2
@@ -15,7 +15,7 @@ public class Question5
      *     > 3
      *     > 4
      *     4
-     * 
+     *
      *    e.g.
      *     > 4
      *     > 2
@@ -25,8 +25,26 @@ public class Question5
      *     2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
-     
+
     Scanner in = new Scanner(System.in);
-    
+    //System.out.print("Enter a integer: ");
+    int times = in.nextInt();
+    int seen = 0;
+    HashMap<Integer, Integer> visited = new HashMap<>();
+    for (int i = 0; i < times; i++) {
+      //System.out.print("Enter a integer: ");
+      int n = in.nextInt();
+      if (visited.containsKey(n)) {
+        visited.put(n, visited.get(n) + 1);
+      } else {
+        visited.put(n, 1);
+      }
+    }
+    for (int x : visited.values()) {
+      if (seen < x) {
+        seen = x;
+      }
+    }
+    System.out.print(seen);
   }
 }
